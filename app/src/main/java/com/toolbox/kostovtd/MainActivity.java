cb8888cacfee19dde63ba1b981ffa22357f9f80f
Todor Kostov <todor.kostov@sirma.bg>
2016-12-01 13:57:31 +0200

    Initial screen navigation included. New dimens resources. New string resources.

--- app/src/main/java/com/toolbox/kostovtd/MainActivity.java
+++ app/src/main/java/com/toolbox/kostovtd/MainActivity.java
@@ -1,8 +1,14 @@
 package com.toolbox.kostovtd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by todor.kostov on 1.12.2016 г..
@@ -11,12 +17,43 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.button_custom_views)
+    Button bCustomViews;
+
+    @BindView(R.id.button_transitions)
+    Button bTransitions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: hit");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.main_screen_title);
+
+        ButterKnife.bind(this);
+
+
+        // CUSTOM VIEWS
+        bCustomViews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to Custom Views Screen
+                Intent customViewsIntent = new Intent(MainActivity.this, CustomViewsActivity.class);
+                startActivity(customViewsIntent);
+            }
+        });
+
+
+        // TRANSITIONS
+        bTransitions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // navigate to Transitions Screen
+                Intent transitionIntent = new Intent(MainActivity.this, TransitionsActivity.class);
+                startActivity(transitionIntent);
+            }
+        });
+
     }
 
 
