7ab5932d92a86522a80ff9293ea0e023c2e35040
Todor Kostov <kostovtd@gmail.com>
2016-12-12 23:34:46 +0200

    ButterKnife included.
    Basic test with UI components.

--- app/src/main/java/com/toolbox/kostovtd/MainActivity.java
+++ app/src/main/java/com/toolbox/kostovtd/MainActivity.java
@@ -3,6 +3,12 @@ package com.toolbox.kostovtd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by todor.kostov on 1.12.2016 г..
@@ -11,12 +17,29 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.main_container)
+    LinearLayout mainContainer;
+
+    @BindView(R.id.button_test)
+    Button bTestScaleXY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: hit");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+        bTestScaleXY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainContainer.setScaleX(0.5f);
+                mainContainer.setScaleY(0.5f);
+            }
+        });
+
     }
 
 
